76bc546a21daafdb1320ed43c93a3f7546c5ad06
Bozhidar Yunchov <bobi.iv.yu@gmail.com>
2021-07-28 10:51:42 +0300

    Updated room filter button.

--- booking/filters.py
+++ booking/filters.py
@@ -15,6 +15,7 @@ class RoomFilter(django_filters.FilterSet):
     capacity = django_filters.NumberFilter(field_name='capacity', lookup_expr='gte', label='Брой гости')
     beds = django_filters.NumberFilter(field_name='beds', lookup_expr='gte', label='Брой легла')
     orderby_price = django_filters.ChoiceFilter(label='Подреди по цена', choices=ORDERBY_PRICE_CHOICES, method='order_by_price')
+    
 
     def order_by_price(self, queryset, name, value):
         expression = 'price' if value == 'asc' else '-price'
